3cc97fc3b6f18d4dde72eace2fbad295a6343ffa
ananicole <ana.nicole02@gmail.com>
2023-08-01 23:29:53 -0300

    fix position tab bar

--- lib/src/features/menu/TabBarComponent.dart
+++ lib/src/features/menu/TabBarComponent.dart
@@ -37,100 +37,105 @@ class _TabBarComponentState extends State<TabBarComponent> {
         ],
       ),
       child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
-          GestureDetector(
-            onTap: () {
-              setState(() {
-                selectedIndex = 0;
-              });
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => AppointmentsPage(),
-                ),
-              );
-            },
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Icon(
-                  Icons.home,
-                  color: selectedIndex == 0 ? Colors.red : Colors.grey,
-                  size: 24,
-                ),
-                Text(
-                  'Página inicial',
-                  style: TextStyle(
+          Expanded(
+            child: GestureDetector(
+              onTap: () {
+                setState(() {
+                  selectedIndex = 0;
+                });
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => AppointmentsPage(),
+                  ),
+                );
+              },
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(
+                    Icons.home,
                     color: selectedIndex == 0 ? Colors.red : Colors.grey,
-                    height: 1.5,
-                    fontSize: 12,
-                    fontFamily: 'Inter',
-                    fontWeight: FontWeight.w400,
+                    size: 24,
                   ),
-                ),
-              ],
+                  Text(
+                    'Página inicial',
+                    style: TextStyle(
+                      color: selectedIndex == 0 ? Colors.red : Colors.grey,
+                      height: 1.5,
+                      fontSize: 12,
+                      fontFamily: 'Inter',
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
-          GestureDetector(
-            onTap: () {
-              setState(() {
-                selectedIndex = 1;
-              });
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => DoadorCardScreen(),
-                ),
-              );
-            },
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Icon(
-                  Icons.search,
-                  color: selectedIndex == 1 ? Colors.red : Colors.grey,
-                  size: 24,
-                ),
-                Text(
-                  'Cartão de Doador',
-                  style: TextStyle(
+          Expanded(
+            child: GestureDetector(
+              onTap: () {
+                setState(() {
+                  selectedIndex = 1;
+                });
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => DoadorCardScreen(),
+                  ),
+                );
+              },
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(
+                    Icons.search,
                     color: selectedIndex == 1 ? Colors.red : Colors.grey,
-                    height: 1.5,
-                    fontSize: 12,
-                    fontFamily: 'Inter',
-                    fontWeight: FontWeight.w400,
+                    size: 24,
                   ),
-                ),
-              ],
+                  Text(
+                    'Cartão de Doador',
+                    style: TextStyle(
+                      color: selectedIndex == 1 ? Colors.red : Colors.grey,
+                      height: 1.5,
+                      fontSize: 12,
+                      fontFamily: 'Inter',
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
-          GestureDetector(
-            onTap: () {
-              setState(() {
-                selectedIndex = 2;
-              });
-              // Implementar a navegação para a tela do perfil aqui
-            },
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Icon(
-                  Icons.person,
-                  color: selectedIndex == 2 ? Colors.red : Colors.grey,
-                  size: 24,
-                ),
-                Text(
-                  'Perfil',
-                  style: TextStyle(
+          Expanded(
+            child: GestureDetector(
+              onTap: () {
+                setState(() {
+                  selectedIndex = 2;
+                });
+                // Implementar a navegação para a tela do perfil aqui
+              },
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(
+                    Icons.person,
                     color: selectedIndex == 2 ? Colors.red : Colors.grey,
-                    height: 1.5,
-                    fontSize: 12,
-                    fontFamily: 'Inter',
-                    fontWeight: FontWeight.w400,
+                    size: 24,
                   ),
-                ),
-              ],
+                  Text(
+                    'Perfil',
+                    style: TextStyle(
+                      color: selectedIndex == 2 ? Colors.red : Colors.grey,
+                      height: 1.5,
+                      fontSize: 12,
+                      fontFamily: 'Inter',
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ],
