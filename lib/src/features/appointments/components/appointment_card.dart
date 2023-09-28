0a7496539912b8e1b8289b891f91e6d4bbd17294
ananicole <ana.nicole02@gmail.com>
2023-09-28 19:06:23 -0300

    get appointment_card.dart

--- lib/src/features/appointments/components/appointment_card.dart
+++ lib/src/features/appointments/components/appointment_card.dart
@@ -1,22 +1,70 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-class AppointmentCard extends StatelessWidget {
+import 'package:shared_preferences/shared_preferences.dart';
+
+class AppointmentCard extends StatefulWidget {
+  @override
+  _AppointmentCardState createState() => _AppointmentCardState();
+}
+
+class _AppointmentCardState extends State<AppointmentCard> {
+  List<Map<String, dynamic>> appointmentDataList = [];
+  bool isLoading = true;
+
+  Future<void> fetchAppointmentData() async {
+    int? userId = await _getUserId();
+
+    final response = await http.get(
+      Uri.parse('http://localhost:8080/appointments/user/$userId'),
+    );
+
+    if (response.statusCode == 200) {
+      final List<dynamic> responseData = json.decode(response.body);
+      setState(() {
+        appointmentDataList = responseData.cast<Map<String, dynamic>>();
+        isLoading = false;
+      });
+    } else {
+      throw Exception('Failed to load appointment data');
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    fetchAppointmentData();
+  }
+
+  Future<int?> _getUserId() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    int? userId = prefs.getInt('userId');
+    return userId;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
       margin: EdgeInsets.all(16),
-      child: Container(
+      child: isLoading
+          ? CircularProgressIndicator()
+          : appointmentDataList.isEmpty
+          ? Center(
+        child: Text('Não há agendamentos para este usuário.'),
+      )
+          : Container(
         width: 335,
         height: 151,
-        child: Padding( // Add Padding widget
-          padding: EdgeInsets.all(12), // Add 12px padding to all sides
+        child: Padding(
+          padding: EdgeInsets.all(12),
           child: Row(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Container(
                 width: 24,
                 height: 24,
-                // Add any content or decoration for the left container
+                // Adicione qualquer conteúdo ou decoração para o contêiner à esquerda
               ),
               Container(
                 width: MediaQuery.of(context).size.width * 0.0828,
@@ -26,7 +74,7 @@ class AppointmentCard extends StatelessWidget {
                   right: MediaQuery.of(context).size.width * 0.084,
                 ),
                 color: Color(0xFFD64545),
-                // Add any content or decoration for the middle container
+                // Adicione qualquer conteúdo ou decoração para o contêiner do meio
               ),
               Expanded(
                 child: Column(
@@ -37,7 +85,7 @@ class AppointmentCard extends StatelessWidget {
                       height: 19,
                       margin: EdgeInsets.only(bottom: 14),
                       child: Text(
-                        'Doação de 01 de março',
+                        'Doação de ${appointmentDataList[0]['date']}',
                         style: TextStyle(
                           fontFamily: 'Inter',
                           fontWeight: FontWeight.w600,
@@ -50,7 +98,7 @@ class AppointmentCard extends StatelessWidget {
                       width: 268,
                       height: 30,
                       child: Text(
-                        'Hemobanco R. Cap. Souza Franco, 290 - Bigorrilho, Curitiba',
+                        'Hemobanco ${appointmentDataList[0]['hemobanco']['address']}, ${appointmentDataList[0]['hemobanco']['city']}',
                         style: TextStyle(
                           fontFamily: 'Inter',
                           fontWeight: FontWeight.w400,
@@ -65,7 +113,10 @@ class AppointmentCard extends StatelessWidget {
                       margin: EdgeInsets.only(top: 12),
                       decoration: BoxDecoration(
                         color: Colors.white,
-                        border: Border.all(width: 1, color: Color(0xFFD64545)),
+                        border: Border.all(
+                          width: 1,
+                          color: Color(0xFFD64545),
+                        ),
                         borderRadius: BorderRadius.circular(100),
                       ),
                       child: Center(
