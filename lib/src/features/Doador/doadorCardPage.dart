368370d89a8ee839598081c4cfdc114517be4977
ananicole <ana.nicole02@gmail.com>
2023-10-04 18:21:19 -0300

    feat: Adiciona imagem em carterinha de doador

--- lib/src/features/Doador/doadorCardPage.dart
+++ lib/src/features/Doador/doadorCardPage.dart
@@ -58,7 +58,8 @@ class _DoadorCardScreenState extends State<DoadorCardScreen> {
                 Navigator.push(
                   context,
                   MaterialPageRoute(
-                    builder: (context) => FullscreenDoadorCardScreen(userData: userData!),
+                    builder: (context) =>
+                        FullscreenDoadorCardScreen(userData: userData!),
                   ),
                 );
               },
@@ -69,62 +70,76 @@ class _DoadorCardScreenState extends State<DoadorCardScreen> {
                   color: Colors.red,
                   borderRadius: BorderRadius.circular(20),
                 ),
-                child: Padding(
-                  padding: EdgeInsets.all(20),
-                  child: userData != null
-                      ? Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            SizedBox(height: 20),
-                            Text(
-                              'Nome: ${userData!.user.name}',
-                              style: TextStyle(
-                                fontSize: 18,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.white,
-                              ),
+                child: Stack(
+                  // Adiciona uma pilha para colocar a imagem em cima do container
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.all(20),
+                      child: userData != null
+                          ? Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          SizedBox(height: 20),
+                          Text(
+                            'Nome: ${userData!.user.name}',
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
                             ),
-                            SizedBox(height: 10),
-                            Text(
-                              'Tipo Sanguíneo: ${userData!.bloodType}',
-                              style: TextStyle(
-                                fontSize: 16,
-                                color: Colors.white,
-                              ),
-                            ),
-                            SizedBox(height: 10),
-                            Text(
-                              'CPF: ${userData!.user.cpf}',
-                              style: TextStyle(
-                                fontSize: 16,
-                                color: Colors.white,
-                              ),
-                            ),
-                            SizedBox(height: 10),
-                            Text(
-                              'Validade: ${userData!.validity}',
-                              style: TextStyle(
-                                fontSize: 16,
-                                color: Colors.white,
-                              ),
-                            ),
-                            SizedBox(height: 10),
-                            Text(
-                              'Número do cartão: ${userData!.cardNumber}',
-                              style: TextStyle(
-                                fontSize: 16,
-                                color: Colors.white,
-                              ),
-                            ),
-                          ],
-                        )
-                      : Text(
-                          'Carregando dados do usuário...',
-                          style: TextStyle(
-                            fontSize: 16,
-                            color: Colors.white,
                           ),
+                          SizedBox(height: 10),
+                          Text(
+                            'Tipo Sanguíneo: ${userData!.bloodType}',
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: Colors.white,
+                            ),
+                          ),
+                          SizedBox(height: 10),
+                          Text(
+                            'CPF: ${userData!.user.cpf}',
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: Colors.white,
+                            ),
+                          ),
+                          SizedBox(height: 10),
+                          Text(
+                            'Validade: ${userData!.validity}',
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: Colors.white,
+                            ),
+                          ),
+                          SizedBox(height: 10),
+                          Text(
+                            'Número do cartão: ${userData!.cardNumber}',
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      )
+                          : Text(
+                        'Carregando dados do usuário...',
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.white,
                         ),
+                      ),
+                    ),
+                    Positioned(
+                      top: 0, // Ajuste a posição vertical conforme necessário
+                      right: 0, // Ajuste a posição horizontal conforme necessário
+                      child: Image.asset(
+                        'gota_de_sangue.png', // Substitua pelo caminho da sua imagem
+                        width: 150, // Ajuste o tamanho conforme necessário
+                        height: 250, // Ajuste o tamanho conforme necessário
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ),
